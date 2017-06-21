01a97d96850cf650e9f68b5cc84461b67c26d9f3
jamesbeltonIBM <james.belton@uk.ibm.com>
2017-06-21 13:44:14 +0100

    Update scripts.js

--- public/myscripts/scripts.js
+++ public/myscripts/scripts.js
@@ -6,7 +6,16 @@ return result;
 
 function getWeather(postcode)
 {
+  var mytext='Result is..... ';
+  
+  if (process.env.VCAP_SERVICES)
+  {
   var vcap_services = JSON.parse(process.env.VCAP_SERVICES);
   var url = vcap_services.weatherinsights[0].credentials.url;
-return url;
+  return mytext+url;
+  };
+  else {
+    return mytext + 'no vcap';
+  };
+  
 };
